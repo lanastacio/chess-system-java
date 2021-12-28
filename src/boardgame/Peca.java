0e12ad0a9bade7d281b3287f1a36440ebafd879a
Lucas Anastacio <lucas_anastacio@hotmail.com>
2021-12-28 14:27:22 -0300

    Iniciando a implementação de peça e tabuleiro

---
+++ src/boardgame/Peca.java
@@ -0,0 +1,18 @@
+package boardgame;
+
+public class Peca {
+
+	protected Position position;
+	private Tabuleiro tabuleiro;
+	
+	public Peca(Tabuleiro tabuleiro) {
+		this.tabuleiro = tabuleiro;
+		position = null;
+	}
+
+	protected Tabuleiro getTabuleiro() {
+		return tabuleiro;
+	}
+	
+	
+}
