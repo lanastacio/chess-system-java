e4d138865b5d4917f47c60c077c1eb04354a383e
Lucas Anastacio <lucas_anastacio@hotmail.com>
2021-12-30 11:22:07 -0300

    Cores nas pecas

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -13,23 +13,34 @@ public class ChessMatch {
 		initialSetup();
 	}
 
-	public ChessPeca[][]getPecas(){
+	public ChessPeca[][] getPecas() {
 		ChessPeca[][] mat = new ChessPeca[tabuleiro.getLinhas()][tabuleiro.getColunas()];
-		for (int i=0; i<tabuleiro.getLinhas(); i++) {
-			for(int j=0; j<tabuleiro.getColunas(); j++) {
-				mat[i][j] = (ChessPeca) tabuleiro.peca(i,j);
+		for (int i = 0; i < tabuleiro.getLinhas(); i++) {
+			for (int j = 0; j < tabuleiro.getColunas(); j++) {
+				mat[i][j] = (ChessPeca) tabuleiro.peca(i, j);
 			}
 		}
 		return mat;
 	}
-	
+
 	private void placeNewPiece(char coluna, int linha, ChessPeca peca) {
 		tabuleiro.lugarPeca(peca, new ChessPosition(coluna, linha).toPosition());
 	}
-	
+
 	private void initialSetup() {
-		placeNewPiece('b', 6, new Torre(tabuleiro, Color.WHITE));
-		placeNewPiece('e', 8, new Rei(tabuleiro, Color.BLACK));
-		placeNewPiece('e', 1, new Rei(tabuleiro, Color.WHITE));
+
+		placeNewPiece('c', 1, new Torre(tabuleiro, Color.WHITE));
+		placeNewPiece('c', 2, new Torre(tabuleiro, Color.WHITE));
+		placeNewPiece('d', 2, new Torre(tabuleiro, Color.WHITE));
+		placeNewPiece('e', 2, new Torre(tabuleiro, Color.WHITE));
+		placeNewPiece('e', 1, new Torre(tabuleiro, Color.WHITE));
+		placeNewPiece('d', 1, new Rei(tabuleiro, Color.WHITE));
+
+		placeNewPiece('c', 7, new Torre(tabuleiro, Color.BLACK));
+		placeNewPiece('c', 8, new Torre(tabuleiro, Color.BLACK));
+		placeNewPiece('d', 7, new Torre(tabuleiro, Color.BLACK));
+		placeNewPiece('e', 7, new Torre(tabuleiro, Color.BLACK));
+		placeNewPiece('e', 8, new Torre(tabuleiro, Color.BLACK));
+		placeNewPiece('d', 8, new Rei(tabuleiro, Color.BLACK));
 	}
 }
